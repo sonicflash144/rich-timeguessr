372744506ccf2ec5caf41501ebf80c1fcb1a5b77
sonicflash144 <pickmealeaf@gmail.com>
2024-04-01 10:54:52 -0500

    fixed maps API load-in

--- components/Map.tsx
+++ components/Map.tsx
@@ -11,36 +11,45 @@ const Map = ({ setGuessLocation }: MapProps) => {
     let marker: google.maps.Marker | null = null;
 
     useEffect(() => {
-        
-        if (!window.google || !mapRef.current) {
-            console.error('Google Maps API failed to load or the map div has not been mounted yet');
-            return;
-        }
-
-        const map = new window.google.maps.Map(mapRef.current, {
-            center: { lat: 29.5984, lng: -95.6226 },
-            zoom: 4,
-            disableDefaultUI: true,
-        });
-
-        map.addListener('click', (e: google.maps.MapMouseEvent) => {
-            setGuessLocation({ lat: e.latLng.lat(), lng: e.latLng.lng() });
-
-            // Remove the old marker if it exists
-            if (marker) {
-                marker.setMap(null);
+        const checkIfGoogleMapsIsLoaded = () => {
+            if (!window.google || !window.google.maps || !window.google.maps.Map || !mapRef.current) {
+                console.error('Google Maps API failed to load or the map div has not been mounted yet');
+                setTimeout(checkIfGoogleMapsIsLoaded, 100);
+            } else {
+                initializeMap();
             }
+        };
 
-            // Create a new marker at the clicked location
-            marker = new window.google.maps.Marker({
-                position: {lat: e.latLng.lat(), lng: e.latLng.lng()},
-                map: map,
-                icon: {
-                    url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-                }
-            });
-        });
-        
+        const initializeMap = () => {
+            const mapElement = mapRef.current;
+            if (mapElement) {
+                const map = new window.google.maps.Map(mapElement, {
+                    center: { lat: 29.5984, lng: -95.6226 },
+                    zoom: 4,
+                    disableDefaultUI: true,
+                });
+
+                map.addListener('click', (e: google.maps.MapMouseEvent) => {
+                    setGuessLocation({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+
+                    // Remove the old marker if it exists
+                    if (marker) {
+                        marker.setMap(null);
+                    }
+
+                    // Create a new marker at the clicked location
+                    marker = new window.google.maps.Marker({
+                        position: {lat: e.latLng.lat(), lng: e.latLng.lng()},
+                        map: map,
+                        icon: {
+                            url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+                        }
+                    });
+                });
+            }
+        };
+
+        checkIfGoogleMapsIsLoaded();
     }, []);
 
     return <div ref={mapRef} className="map" />;
